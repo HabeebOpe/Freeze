6d90a4630fe4d440f417fbc35f5d3ace2f536667
habeebullahokunlola@gmail.com <habeebullahokunlola@gmail.com>
2023-03-10 23:32:33 +0100

    v-0.03

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const body = document.querySelector("body")
-const searchClick = document.querySelector("#search-click")
+const searchClick = document.querySelectorAll("#search-click")
 const navClick = document.querySelector("#nav-click")
 
 /*------------NAVIGATION DISPLAY------------*/
@@ -8,13 +8,26 @@ navClick.addEventListener("click", ()=>{
 })
 
 /*-----------SEAERCH BAR DISPLAY------------*/
-searchClick.addEventListener("click", ()=>{
-  body.classList.toggle("show-search")
+searchClick.forEach(searchClicki =>{
+  searchClicki.addEventListener("click", ()=>{
+    body.classList.toggle("show-search")
+  })
 })
 
 /*----------NAV DISPLAY SCRIPT--------------*/
+
+
 function scroll(){
    var  scrollValue = window.scrollY;
+   
+   if(scrollValue--){
+     body.classList.remove("show-nav")
+     body.classList.remove("retweetpop")
+   }
+   if(scrollValue++){
+     body.classList.remove("show-nav")
+     body.classList.remove("retweetpop")
+   }
    const btn = document.querySelector(".btn");
    if(scrollValue < 150){
        body.classList.remove("navdis")
@@ -22,6 +35,13 @@ function scroll(){
    else{
        body.classList.add("navdis")
    }
+   if(scrollValue > 150){
+     body.classList.add("shad")
+   }
+   else{
+     body.classList.remove("shad")
+   }
+   
 }
 window.addEventListener("scroll", scroll)
 
@@ -38,6 +58,10 @@ img.forEach(image =>{
     body.classList.toggle('review-mag');
     body.classList.toggle("stiff")
     mage.src = cla
+    if(image.classList.contains("logoimg")){
+      body.classList.remove('review-mag');
+      body.classList.remove("stiff")
+    }
   })
 })
 
@@ -48,16 +72,37 @@ openMessage.forEach(openMess =>{
     if(body.classList.contains("open-notification")){
     body.classList.remove("open-notification")
     }
+    if(body.classList.contains("open-trend")){
+    body.classList.remove("open-trend")
+    }
     body.classList.toggle("open-message")
     body.classList.add("change")
   })
 })
+/*--------------FOR YOU OPEN---------------*/
+const openTrending = document.querySelectorAll(".trending-open")
+openTrending.forEach(openTrend =>{
+  openTrend.addEventListener("click", ()=>{
+    if (body.classList.contains("open-notification")) {
+      body.classList.remove("open-notification")
+    }
+    if (body.classList.contains("openMessage")) {
+      body.classList.remove("openMessage")
+    }
+    body.classList.toggle("open-trend")
+  })
+})
+
+
 
 /*------------NOTIFICATION OPEN-------------*/
 const openNotification = document.querySelector(".notification-open")
 openNotification.addEventListener("click", ()=>{
   if(body.classList.contains("open-message")){
     body.classList.remove("open-message")
+  }
+  if(body.classList.contains("open-trend")){
+    body.classList.remove("open-trend")
   }
   body.classList.toggle("open-notification")
 })
@@ -70,6 +115,9 @@ homeClick.addEventListener("click", ()=>{
   }
   if(body.classList.contains("open-notification")){
     body.classList.remove("open-notification")
+   }
+   if(body.classList.contains("open-trend")){
+    body.classList.remove("open-trend")
    }
 })
 
@@ -109,9 +157,115 @@ theme.forEach(themeBtn =>{
 })
 
 
+/*-------------SCROLL TO REMOVE THE SIDENAV----------------*/
+
+const redi = document.querySelectorAll(".love");
+const medi = document.querySelectorAll(".message");
+const retwi = document.querySelectorAll(".retweet");
+const shari = document.querySelectorAll(".share");
+
+const rered = document.querySelectorAll(".fa-heart")
+const reblue = document.querySelectorAll(".fa-message")
+const regreen = document.querySelectorAll(".fa-retweet")
+const repurple = document.querySelectorAll(".fa-share")
 
 
 
+redi.forEach(redid =>{
+  redid.addEventListener("click", (e)=>{
+    e.target.classList.toggle("red")
+    rered.forEach(reredi =>{
+      reredi.classList.remove("red")
+    })
+    if(e.target.classList.contains("red")){
+      e.target.classList.toggle("resize")
+    }
+    else{
+      e.target.classList.remove("resize")
+    }
+
+  })
+})
+medi.forEach(medid =>{
+  medid.addEventListener("click", (e)=>{
+    e.target.classList.toggle("blue")
+    reblue.forEach(rebluei =>{
+      rebluei.classList.remove("blue")
+    })
+    if(e.target.classList.contains("blue")){
+      e.target.classList.toggle("resize")
+    }
+    else{
+      e.target.classList.remove("resize")
+    }
+  })
+})
+
+const retweetclick = document.querySelectorAll(".retweetclick")
+
+retwi.forEach(retwid =>{
+  retwid.addEventListener("click", (e)=>{
+    e.target.classList.toggle("retclicked")
+    
+    regreen.forEach(regreeni =>{
+      regreeni.classList.remove("green")
+   
+    /*--------------RETWEET POPUP BOX ---------------*/
+    body.classList.toggle("retweetpop")
+    regreeni.classList.remove("green")
+    
+    
+    retweetclick.forEach(retweetclicki => {
+      retweetclicki.addEventListener("click", () => {
+        regreeni.classList.remove("green")
+        e.target.classList.add("green")
+      })
+    })
+    regreeni.classList.remove("green")
+    if (e.target.classList.contains("green"))
+    {
+      regreeni.classList.remove("green")
+      body.classList.remove("retweetpop")
+      e.target.classList.remove("green")
+    }
+    else {
+      regreeni.classList.remove("green")
+      body.classList.add("retweetpop")
+      e.target.classList.add("green")
+    }
+    
+    regreeni.classList.remove("green")
+    
+    })
+    if (e.target.classList.contains("retclicked")) {
+      regreeni.classList.remove("green")
+      if (e.target.classList.contains("green")) {
+        e.target.classList.toggle("resize")
+      }
+      else {
+        regreeni.classList.remove("green")
+        e.target.classList.remove("resize")
+      }
+    }
+    
+    
+    
+  })
+})
+shari.forEach(sharid =>{
+  sharid.addEventListener("click", (e)=>{
+    e.target.classList.toggle("purple")
+    repurple.forEach(reshari =>{
+      reshari.classList.remove("purple")
+    })
+    if(e.target.classList.contains("purple")){
+      e.target.classList.toggle("resize")
+    }
+    else{
+      e.target.classList.remove("resize")
+    }
+  })
+})
 
 
 
